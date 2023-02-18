275d66d28bb7af93fa167bf00dd32797a61e9afd
Javier Moreno <ejmoreno6@espe.edu.ec>
2023-02-17 23:25:23 -0500

    update settings.py

--- puddle/settings.py
+++ puddle/settings.py
@@ -130,8 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = 'media'
-MEDIA_ROOT = '/media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
